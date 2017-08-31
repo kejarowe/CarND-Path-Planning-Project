f80290adaf57aaf9ecaaf10a149237c1f0f18b75
Keja Rowe <keja.rowe@gmail.com>
2017-08-31 16:29:33 -0700

    first working version of lane following + speed adjustment which doesn't violate jerk or accel constraints

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <map>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -18,6 +20,12 @@ using json = nlohmann::json;
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+
+struct map_data {
+  std::vector<double> * x;
+  std::vector<double> * y;
+  std::vector<double> * s;
+};
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -159,6 +167,153 @@ vector<double> getXY(double s, double d, vector<double> maps_s, vector<double> m
 
 }
 
+void next_s(double &current_s, double dt = 0.02)
+{
+  static double current_speed = 0;
+  static double current_accel = 0;
+  const double target_speed = 6.5; //about 15 mph for now
+  const double target_accel = 5;
+  const double allowed_jerk = 5;
+
+  current_s += current_speed * dt;
+  current_speed = current_speed >= target_speed ? current_speed : current_speed + current_accel * dt;
+  current_accel = current_accel >= target_accel ? current_accel : current_accel + allowed_jerk * dt;
+  //current_speed = current_speed >= target_speed ? target_speed : current_speed + current_accel * dt;
+  //current_accel = current_accel >= target_accel ? target_accel : current_accel + allowed_jerk * dt;
+}
+
+void next_s(double &current_s, double &current_speed, double &current_accel, double dt = 0.02)
+{
+  const double target_speed = 6.5; //about 15 mph for now
+  const double target_accel = 5;
+  const double allowed_jerk = 5;
+
+  current_s += current_speed * dt;
+  current_speed = current_speed >= target_speed ? current_speed : current_speed + current_accel * dt;
+  current_accel = current_accel >= target_accel ? current_accel : current_accel + allowed_jerk * dt;
+  //current_speed = current_speed >= target_speed ? target_speed : current_speed + current_accel * dt;
+  //current_accel = current_accel >= target_accel ? target_accel : current_accel + allowed_jerk * dt;
+}
+
+void next_x(double &current_x, std::map<double,double> &arc_length_map, double dt = 0.02)
+{
+  static double current_speed = 0;
+  static double current_accel = 0;
+  const double target_speed = 6.5; //about 15 mph for now
+  const double target_accel = 5;
+  const double allowed_jerk = 5;
+
+  current_x += current_speed * dt;
+  current_speed = current_speed >= target_speed ? current_speed : current_speed + current_accel * dt;
+  current_accel = current_accel >= target_accel ? current_accel : current_accel + allowed_jerk * dt;
+  //current_speed = current_speed >= target_speed ? target_speed : current_speed + current_accel * dt;
+  //current_accel = current_accel >= target_accel ? target_accel : current_accel + allowed_jerk * dt;
+}
+
+inline void next_xy(double target_speed, double target_d, double &pos_x, double &pos_y, double &pos_theta, map_data &md, double dt = 0.02)
+{
+  static double current_speed = 0;
+  static double current_accel = 0;
+  static double current_theta = 0;
+  static double max_d_theta = pi(); 
+  static double allowed_lon_accel = 5;
+  static double allowed_jerk = 5;
+  const  double allowed_lat_accel = 5;
+  const  double projection_multiplier = 5; //10 //5
+  const  double projection_offset = 5; //15
+
+  //limit centripital acceleration
+  if (current_speed > 0)
+    max_d_theta = allowed_lat_accel * dt / current_speed;
+
+  double speed_delta = target_speed - current_speed;
+  allowed_lon_accel = std::copysign(allowed_lon_accel,speed_delta);
+  allowed_jerk = std::copysign(allowed_jerk,speed_delta);
+
+  double current_dist = current_speed * dt;
+  double look_ahead_distance = current_dist * projection_multiplier + projection_offset; 
+  //current_speed = current_speed >= target_speed ? current_speed : current_speed + current_accel * dt;
+  //current_accel = current_accel >= allowed_lon_accel ? current_accel : current_accel + allowed_jerk * dt;
+  
+  //set jerk
+  double desired_accel = 0; 
+  if (std::abs(1.0 / dt * speed_delta) <= allowed_lon_accel ){
+    //we can reach our desired velocity in the next time step
+    desired_accel = 1.0 / dt * speed_delta;
+  }
+  else if (std::abs(speed_delta) <= 0.5 * std::pow(current_accel,2) / allowed_jerk) {
+    //max jerk towards 0 so that we reach our speed our accel will be 0
+    desired_accel = 0;
+  }
+  else {
+    //max acceleration in the direction of desired speed
+    desired_accel = std::copysign(allowed_lon_accel,speed_delta);
+  }
+
+  double delta_accel = desired_accel - current_accel;
+  double jerk_command = std::copysign(std::min(allowed_jerk,std::abs(delta_accel)),delta_accel);
+
+
+  current_speed += current_accel * dt;
+  current_accel += jerk_command * dt;
+  
+  vector<double> sd_vec = getFrenet(pos_x, pos_y, pos_theta, *md.x, *md.y);
+  vector<double> next_xy = getXY(sd_vec[0] + look_ahead_distance, target_d, *md.s, *md.x, *md.y);
+
+  current_theta = std::atan2(next_xy[1] - pos_y, next_xy[0] - pos_x);
+  double d_theta = current_theta - pos_theta;
+  //fix angle wrap around
+  while (d_theta > M_PI)
+    d_theta -= 2*M_PI;
+  while(d_theta < -M_PI)
+    d_theta += 2*M_PI;
+  pos_theta += std::abs(d_theta) > max_d_theta ? std::copysign(max_d_theta,d_theta) : d_theta;
+  pos_x += current_dist * cos(pos_theta);
+  pos_y += current_dist * sin(pos_theta);
+}
+
+bool cipv(double ego_s, double ego_d, std::vector<std::vector<double>> &car_list, double &best_ds, double &best_speed)
+{
+  static double d_tolerance  = 1.5;
+  best_ds = std::numeric_limits<double>::infinity();
+  bool result = false;
+  for (auto car : car_list) {
+    if (car[5] > ego_s /*in front of ego car*/ 
+      && std::abs(car[6]-ego_d) < d_tolerance /*car in same lane*/) {
+
+      double ds = car[5] - ego_s;
+      if (ds < best_ds) {
+        result = true;
+        best_ds = ds;
+        best_speed = std::sqrt(std::pow(car[3],2) + std::pow(car[4],2));
+      }
+    }
+  }
+  return result;
+}
+
+void traj_fsm(double target_vehicle_speed, double target_vehicle_ds, double &desired_ego_speed, double &desired_ego_d)
+{
+  const double psuedo_target_headway_threshold = 5.0; //seconds
+  const double set_speed = 20.1; //45mph in mps
+  const double lane_width = 4; //meters
+  static int desired_lane = 1; //leftmost lane: 0, middle_lane: 1, right_lane: 2
+
+  double psuedo_target_headway = target_vehicle_ds / set_speed;
+
+  if (psuedo_target_headway < psuedo_target_headway_threshold) {
+    //match target speed since its close to us
+    desired_ego_speed = std::min(target_vehicle_speed,set_speed);
+  }
+  else {
+    //no nearby targets in our lane
+    desired_ego_speed = set_speed;
+  }
+
+  //just stay in middle lane for now
+  desired_ego_d = (desired_lane + 0.5) * lane_width;
+}
+
 int main() {
   uWS::Hub h;
 
@@ -168,6 +323,11 @@ int main() {
   vector<double> map_waypoints_s;
   vector<double> map_waypoints_dx;
   vector<double> map_waypoints_dy;
+
+  map_data md;
+  md.x = &map_waypoints_x;
+  md.y = &map_waypoints_y;
+  md.s = &map_waypoints_s;
 
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
@@ -196,7 +356,7 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&md](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -222,6 +382,11 @@ int main() {
           	double car_d = j[1]["d"];
           	double car_yaw = j[1]["yaw"];
           	double car_speed = j[1]["speed"];
+            static double last_speed = car_speed;
+            std::chrono::time_point<std::chrono::system_clock> current_time = std::chrono::system_clock::now();
+            static std::chrono::time_point<std::chrono::system_clock> last_time = current_time;
+            double dt = (current_time - last_time).count();
+            double car_accel = dt > 0 ? (car_speed - last_speed) / dt : 0;
 
           	// Previous path data given to the Planner
           	auto previous_path_x = j[1]["previous_path_x"];
@@ -232,12 +397,87 @@ int main() {
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
+            std::vector<std::vector<double>> car_list = sensor_fusion;
 
           	json msgJson;
 
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+            /*
+              Trajectory Planning Strategy
+              -find cipv by parsing fused object list
+              -send cipv to decision maker which will determine desired speed and d
+              -send desired speed and d to next_xy
+              -next_xy calculates point to send to dynamic controller
+            */
+
+            //trajectory generation vars
+            double pos_x, pos_y, pos_theta, pos_s, pos_d,
+              pos_speed, pos_accel, desired_d, desired_speed;
+            std::vector<double> spline_input_x;
+            std::vector<double> spline_input_y;
+            
+            //copy old trajectory
+            int path_size = previous_path_x.size();
+            for(int i = 0; i < path_size; i++)
+            {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+            
+            //get last point on old trajectory
+            if(path_size == 0)
+            {
+              pos_x = car_x;
+              pos_y = car_y;
+              pos_s = car_s;
+              pos_d = car_d;
+              pos_theta = 0;
+              pos_speed = car_speed;
+              pos_accel = car_accel;
+            }
+            else
+            {
+              pos_x = next_x_vals[path_size-1];
+              pos_y = next_y_vals[path_size-1];
+              if (path_size > 1)
+                pos_theta = std::atan2(pos_y - next_y_vals[path_size-2], pos_x - next_x_vals[path_size-2]);
+              else
+                pos_theta = 0;
+              vector<double> sd_vec = getFrenet(pos_x, pos_y, pos_theta, map_waypoints_x, map_waypoints_y);
+              pos_s = sd_vec[0];
+              pos_d = sd_vec[1];
+              pos_speed = car_speed;
+              pos_accel = car_accel;
+            }
+
+            //generate new trajectory points
+
+            //find cipv
+            double target_vehicle_ds, target_vehicle_speed;
+            
+            bool success = cipv(pos_s, pos_d, car_list, target_vehicle_ds, target_vehicle_speed); 
+        
+            //run fsm
+            traj_fsm(target_vehicle_speed, target_vehicle_ds, desired_speed, desired_d);
+            printf("target_vehicle_speed: %lf, target_vehicle_ds: %lf, desired_speed: %lf\n",
+              target_vehicle_speed, target_vehicle_ds, desired_speed);
+
+            double next_x, next_y;
+            next_x = pos_x;
+            next_y = pos_y;
+            for(int i = 0; i < 50-path_size; i++)
+            { 
+              next_xy(desired_speed, desired_d, pos_x, pos_y, pos_theta, md);
+              //convert pos
+              next_x_vals.push_back(pos_x);
+              next_y_vals.push_back(pos_y);
+            }
+
+            last_speed = car_speed;
+            last_time = current_time;
+            //end path generation
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
